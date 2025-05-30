c6d8d0d66453aaebfc86348a9169a3f811169adc
cheikabdoulaye <cheikamirg@gmail.com>
2025-05-30 10:27:15 +0000

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,18 @@
+module.exports = (req, res) => {
+  const VERIFY_TOKEN = "pagebot"; // Le même que dans Meta
+
+  if (req.method === "GET") {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+};
